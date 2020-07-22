b005468b53f7bebffe3f5bfe81e92c94094556d4
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-22 10:11:03 +0300

    Fixed Url path

--- groups/urls.py
+++ groups/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/<int:pk>/', views.CreateGroup.as_view(), name='create'),
     path('posts/<int:pk>/', views.SingleGroup.as_view(), name='single'),
-    path(r'join/(?P<slug>[-\w]+)/$', views.JoinGroup.as_view(), name='join'),
-    path(r'leave/(?P<slug>[-\w]+)/$', views.LeaveGroup.as_view(), name='Leave'),
+    path('join/(<slug>)', views.JoinGroup.as_view(), name='join'),
+    path('leave/(<slug>)', views.LeaveGroup.as_view(), name='Leave'),
 ]
