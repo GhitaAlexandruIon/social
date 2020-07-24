df05feeba757ca8e14e045a9f4fc281c59b1e5e8
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-07-24 12:52:02 +0300

    Merge branch 'master' into dev
    # Conflicts:
    #	accounts/__pycache__/admin.cpython-37.pyc
    #	accounts/__pycache__/urls.cpython-37.pyc
    #	groups/urls.py
    #	social/__pycache__/settings.cpython-37.pyc
    #	templates/base.html

--- groups/urls.py
+++ groups/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/', views.CreateGroup.as_view(), name='create'),
     path('<slug>/', views.SingleGroup.as_view(), name='single'),
-    path(r'join/(?P<slug>[-\w]+)/$', views.JoinGroup.as_view(), name='join'),
-    path(r'leave/(?P<slug>[-\w]+)/$', views.LeaveGroup.as_view(), name='Leave'),
+    path('join/(<slug>)', views.JoinGroup.as_view(), name='join'),
+    path(r'leave/(<slug>)', views.LeaveGroup.as_view(), name='Leave'),
 ]
